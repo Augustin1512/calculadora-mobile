bfd1fcd352baf9ed5e98dc619d82959d2d0f60f9
Augustin1512 <augustosilvagomes1512@gmail.com>
2024-08-24 10:49:25 +0900

    Project's Initial Commit

---
+++ www/components/loader.js
@@ -0,0 +1,11 @@
+/**
+ * This is an auto-generated code by Monaca JS/CSS Components.
+ * Please do not edit by hand.
+ */
+
+/*** <GENERATED> ***/
+
+$(document).on("click",".botao",function(){
+    textoDisplay += $(this).val();
+
+
